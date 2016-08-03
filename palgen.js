5c80c0b9542a6a35767304eeec490f966ab5b413
Stephen Lavelle <analytic@gmail.com>
2016-08-03 01:10:18 +0100

    blep

--- palgen.js
+++ palgen.js
@@ -1,6 +1,68 @@
 var diff = require('color-diff');
 var fs = require('fs');
 
+function lab_to_xyz (lab) {
+	var l = lab.L;
+	var a = lab.a;
+	var b = lab.b;
+	var x;
+	var y;
+	var z;
+
+	y = (l + 16) / 116;
+	x = a / 500 + y;
+	z = y - b / 200;
+
+	var y2 = Math.pow(y, 3);
+	var x2 = Math.pow(x, 3);
+	var z2 = Math.pow(z, 3);
+	y = y2 > 0.008856 ? y2 : (y - 16 / 116) / 7.787;
+	x = x2 > 0.008856 ? x2 : (x - 16 / 116) / 7.787;
+	z = z2 > 0.008856 ? z2 : (z - 16 / 116) / 7.787;
+
+	x *= 95.047;
+	y *= 100;
+	z *= 108.883;
+
+	return [x, y, z];
+};
+
+function xyz_to_rgb(xyz){
+	var x = xyz[0] / 100;
+	var y = xyz[1] / 100;
+	var z = xyz[2] / 100;
+	var r;
+	var g;
+	var b;
+
+	r = (x * 3.2406) + (y * -1.5372) + (z * -0.4986);
+	g = (x * -0.9689) + (y * 1.8758) + (z * 0.0415);
+	b = (x * 0.0557) + (y * -0.2040) + (z * 1.0570);
+
+	// assume sRGB
+	r = r > 0.0031308
+		? ((1.055 * Math.pow(r, 1.0 / 2.4)) - 0.055)
+		: r * 12.92;
+
+	g = g > 0.0031308
+		? ((1.055 * Math.pow(g, 1.0 / 2.4)) - 0.055)
+		: g * 12.92;
+
+	b = b > 0.0031308
+		? ((1.055 * Math.pow(b, 1.0 / 2.4)) - 0.055)
+		: b * 12.92;
+
+	r = Math.min(Math.max(0, r), 1);
+	g = Math.min(Math.max(0, g), 1);
+	b = Math.min(Math.max(0, b), 1);
+
+	return {R:r * 255,G: g * 255,B: b * 255};
+}
+
+function lab_to_rgb (lab){
+	return xyz_to_rgb(lab_to_xyz(lab));
+}
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + Math.floor(min);
 }
@@ -48,13 +110,22 @@ function score(pal){
 function convert(pal){
 	var result = [];
 	for (var i=0;i<16;i++){
-		result.push(diff.rgb_to_lab(pal[i]));
+		if (i===0){
+			pal[i]={R:0,G:0,B:0};
+		} else if (i==15){
+			pal[i]={R:255,G:255,B:255};		
+		}
+		var c = diff.rgb_to_lab(pal[i])
+		c['L']=i*100/15;
+
+		pal[i] = lab_to_rgb(c);
+		result.push(c);
 	}
 	return result;
 }
 
 function componentToHex(c) {
-    var hex = c.toString(16);
+    var hex = Math.floor(c).toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
